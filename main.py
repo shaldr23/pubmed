0f8ab30b2e9d8c59236ac2625351dab12dd0b5bb
Alexander Shaposhnikov <shaldr23@gmail.com>
2020-12-23 18:34:41 +0300

    ability to search by keywords, fetch and save into text file

--- main.py
+++ main.py
@@ -1,12 +1,73 @@
 # %%
 import json
 import requests
+import time
 
+
+TIME_BETWEEN_REQUESTS = .15
 TERM = 'SARS-CoV-2 OR COVID-19'
+REQUEST_TRIES = 3
+TIME_BETWEEN_TRIES = 5
+PRIMARY_OUTPUT_FILE = './output/results.medline'
+
+search_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?'
+fetch_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?'
+
+
+def make_request(basic_url, params):
+    """
+    Make request (esearch or efetch).
+    Makes several tries of a request if status code is not 200.
+    Keeps request rate limitation.
+    Returns request object.
+    """
+    params = params.copy()
+    params.update({'db': 'pubmed',
+                   'retmode': 'json'})
+    for i in range(REQUEST_TRIES):
+        if time.time() - make_request.last_request_time < TIME_BETWEEN_REQUESTS:
+            time.sleep(TIME_BETWEEN_REQUESTS)
+        make_request.last_request_time = time.time()
+        req = requests.get(basic_url, params=params)
+        if req.status_code == 200:
+            return req
+            break
+        else:
+            time.sleep(TIME_BETWEEN_TRIES)
+    raise ValueError('Wrong request results.')
+
+
+make_request.last_request_time = 0
+
 with open('user_key.json') as key_file:
     user_data = json.load(key_file)
 
-basic_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?'
-params = {'db': 'pubmed'}
-params.update(user_data)
-req = requests.get(basic_url, params=params)
+# esearch
+search_params = {'term': TERM,
+                 'db': 'pubmed',
+                 'retmode': 'json',
+                 'usehistory': 'y'}
+search_params.update(user_data)
+req = make_request(search_url, search_params)
+search_result = req.json()['esearchresult']
+webenv = search_result['webenv']
+count = int(search_result['count'])
+key = search_result['querykey']
+
+# efetch
+retmax = 10**4
+with open(PRIMARY_OUTPUT_FILE, 'w') as output:
+    for cycle, retstart in enumerate(range(0, count, retmax), start=1):
+        fetch_params = {'db': 'pubmed',
+                        'WebEnv': webenv,
+                        'query_key': key,
+                        'retstart': retstart,
+                        'retmax': retmax,
+                        'rettype': 'medline'}
+        fetch_params.update(user_data)
+        data = make_request(fetch_url, fetch_params).text
+        output.write(data)
+        articles_count = data.count('\n\n') + 1
+        print(f'Cycle № {cycle}, articles count: {articles_count}')
+
+# %%
