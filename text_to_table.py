35100ab6e410c6ba5a2293ec4f54fe22af8172dc
Alexander Shaposhnikov <shaldr23@gmail.com>
2020-12-28 00:47:32 +0300

    makes dataframe from medline text

--- text_to_table.py
+++ text_to_table.py
@@ -2,25 +2,40 @@
 import pandas as pd
 import re
 
-MEDLINE_TEXT_FILE = '.data/output/results.medline'
+MEDLINE_TEXT_FILE = './data/output/results.medline'
 
 
 def processrecord(record: str,
-                  exclude_fields=['AU', 'FAU', 'AD']) -> 'pd.Series':
+                  fields=['PMID', 'DP', 'TI', 'BTI',
+                          'AB', 'PT', 'OT']) -> 'pd.Series':
     """
     One record into Series
     """
     record = record.strip()
+    record = record.replace('\n' + ' '*6, ' ')
     result = pd.Series()
-    re = r'^\w{2,4}'  # to correct
-    for line in record:
-        field = record[:4]
-        value = record[7:]
+    for line in record.split('\n'):
+        field = line[:4].strip()
+        value = line[6:]
+        if field in fields:
+            if field not in result.index:
+                result[field] = value
+            else:
+                if type(result[field]) != list:
+                    result[field] = [result[field]]
+                result[field].append(value)
+    return result
 
 
-record = ''
+records_list = []
 with open(MEDLINE_TEXT_FILE) as mtf:
-    line = mtf.readline()
-    record += line
-    if line == '\n':
-        processrecord(record)
+    record = ''
+    for line in mtf:
+        record += line
+        if line == '\n' and record != '\n':
+            records_list.append(processrecord(record))
+            record = ''
+if record:
+    records_list.append(processrecord(record))  # catch last record
+
+frame = pd.DataFrame(records_list)
